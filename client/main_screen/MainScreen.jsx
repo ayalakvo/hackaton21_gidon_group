605e7640d2eb061586375f8c85aa0bfa1db419f8
noy <noytsolshain@carmel6000.amitnet.org>
2021-06-09 13:47:59 +0300

    add the template of the main page

---
+++ client/main_screen/MainScreen.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
+
+//styles:
+import styles from './MainScreenStyle'
+
+
+
+const MainScreen = ({ route, navigation }) => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+export default MainScreen
