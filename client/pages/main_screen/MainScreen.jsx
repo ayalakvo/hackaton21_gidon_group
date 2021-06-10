3c1141bd04388bd68a2e272ce29005a50e65f4c8
Aya Israeli <ayaisraeli@carmel6000.amitnet.org>
2021-06-10 09:11:49 +0300

    save time between videos

--- client/pages/main_screen/MainScreen.jsx
+++ client/pages/main_screen/MainScreen.jsx
@@ -13,7 +13,7 @@ import { vh, vw } from 'react-native-expo-viewport-units';
 const MainScreen = ({ route, navigation }) => {
     const [showLoading, setShowLoading] = useState(false)
     const [showMen, setShowMen] = useState(true)
-    const [camera, setCamera] = useState(1)
+    const [camera, setCamera] = useState({ num: 1, time: 0 })
     const video = React.useRef(null);
     const [status, setStatus] = React.useState({ shouldPlay: true });
     const opacityAnim = useRef(new Animated.Value(0)).current
@@ -56,12 +56,15 @@ const MainScreen = ({ route, navigation }) => {
             menu.show();
     };
     const changeCamera = (num) => {
-        timer.current = setTimeout(closeMen, 5000);
-        setCamera(num);
         hideMenu();
+        (async () => {
+            let n = await video.current.getStatusAsync()
+            timer.current = setTimeout(closeMen, 5000);
+            setCamera({ num: num, time: n.positionMillis });
+        })();
     }
     return (
-        <TouchableWithoutFeedback onPress={() => { setShowMen(prev => !prev) }}>
+        <TouchableWithoutFeedback onPress={() => { setShowMen(prev => !prev); }}>
             <View style={{ alignItems: "center" }}>
                 <StatusBar hidden />
                 <Animated.View style={{ opacity: opacityAnim, position: "absolute", zIndex: 1, }}>
@@ -69,7 +72,7 @@ const MainScreen = ({ route, navigation }) => {
                         <Menu
                             ref={setMenuRef}
                             button={<View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center" }}><Text onPress={showMenu} style={styles.button}
-                            >Camera {camera}</Text><Image source={require('../../assets/arrow4.png')} style={{ width: vh(8), height: vh(9), zIndex: 10, position: "absolute", left: vw(18) }} /></View>}
+                            >Camera {camera.num}</Text><Image source={require('../../assets/arrow4.png')} style={{ width: vh(8), height: vh(9), zIndex: 10, position: "absolute", left: vw(18) }} /></View>}
                             style={{ marginTop: vh(9), marginLeft: vh(-2) }}
                         >
                             <MenuItem onPress={() => { changeCamera(1) }}>Camera 1</MenuItem>
@@ -90,7 +93,7 @@ const MainScreen = ({ route, navigation }) => {
                         </TouchableOpacity>
                     </View>
                 </Animated.View>
-                {camera === 1 && <Video
+                {camera.num === 1 && <Video
                     onLoad={() => setShowLoading(false)}
                     onLoadStart={() => setShowLoading(true)}
                     ref={video}
@@ -99,23 +102,26 @@ const MainScreen = ({ route, navigation }) => {
                     useNativeControls
                     resizeMode="contain"
                     shouldPlay={true}
+                    positionMillis={camera.num == 1 ? camera.time : null}
                     onPlaybackStatusUpdate={status => setStatus(() => { return { shouldPlay: true } })}
                 />}
-                {camera === 2 && <Video
+                {camera.num === 2 && <Video
                     onLoad={() => setShowLoading(false)}
                     onLoadStart={() => setShowLoading(true)}
                     ref={video}
                     style={styles.video}
                     source={require('../../assets/Movie2.mp4')}
+                    positionMillis={camera.num == 2 ? camera.time : null}
                     useNativeControls
                     resizeMode="contain"
                     shouldPlay={true}
                     onPlaybackStatusUpdate={status => setStatus(() => { return { shouldPlay: true } })}
                 />}
-                {camera === 3 && <Video
+                {camera.num === 3 && <Video
                     onLoad={() => setShowLoading(false)}
                     onLoadStart={() => setShowLoading(true)}
                     ref={video}
+                    positionMillis={camera.num == 3 ? camera.time : null}
                     style={styles.video}
                     source={require('../../assets/Movie3.mp4')}
                     useNativeControls
@@ -123,10 +129,11 @@ const MainScreen = ({ route, navigation }) => {
                     shouldPlay={true}
                     onPlaybackStatusUpdate={status => setStatus(() => { return { shouldPlay: true } })}
                 />}
-                {camera === 4 && <Video
+                {camera.num === 4 && <Video
                     onLoad={() => setShowLoading(false)}
                     onLoadStart={() => setShowLoading(true)}
                     ref={video}
+                    positionMillis={camera.num == 4 ? camera.time : null}
                     style={styles.video}
                     source={require('../../assets/Movie4.mp4')}
                     useNativeControls
